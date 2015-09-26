57eb7f5eaa8f2a90f72e519ae7c974b246a93f61
Deena Wang <thedeenawang@gmail.com>
2015-09-25 21:40:49 -0400

    bg squeaking loops forever

--- libs/infiniFuck.js
+++ libs/infiniFuck.js
@@ -1,8 +1,12 @@
 $(function() {
+  var MORTY_FILES = ['mortyclimax.mp3', 'mortyexhale1.mp3'];
+
   'use strict';
   var bgId = 'bedsqueak';
   createjs.Sound.on('fileload', function(event) {
     var bg = createjs.Sound.play(event.id);
+    console.log(bg);
+    // bg.sourceNode.playbackRate.value = 2
     bg.loop = -1;
   });
 
