b7159775b77af7031fe912453969b5e772e59c0a
Deena Wang <thedeenawang@gmail.com>
2015-09-25 22:01:07 -0400

    use queue to load shit

--- libs/infiniFuck.js
+++ libs/infiniFuck.js
@@ -1,14 +1,25 @@
 $(function() {
-  var MORTY_FILES = ['mortyclimax.mp3', 'mortyexhale1.mp3'];
-
   'use strict';
-  var bgId = 'bedsqueak';
-  createjs.Sound.on('fileload', function(event) {
-    var bg = createjs.Sound.play(event.id);
-    console.log(bg);
-    // bg.sourceNode.playbackRate.value = 2
-    bg.loop = -1;
+  var MORTY_FILES = ['mortyclimax.mp3', 'mortyexhale1.mp3'];
+  var queue = new createjs.LoadQueue(true);
+  queue.installPlugin(createjs.Sound);
+
+  for (var i = MORTY_FILES.length - 1; i >= 0; i--) {
+    queue.loadFile({id: MORTY_FILES[i], src: 'assets/' + MORTY_FILES[i]});
+  }
+
+  queue.on('complete', function() {
   });
 
-  createjs.Sound.registerSound('assets/bedsqueak2.mp3', bgId);
+  queue.load();
+
+  // var bgId = 'bedsqueak';
+  // createjs.Sound.on('fileload', function(event) {
+  //   var bg = createjs.Sound.play(event.id);
+  //   console.log(bg);
+  //   // bg.sourceNode.playbackRate.value = 2
+  //   bg.loop = -1;
+  // });
+
+  // createjs.Sound.registerSound('assets/bedsqueak2.mp3', bgId);
 });
